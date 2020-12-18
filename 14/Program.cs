621e7e5aca39b2af7f952a75695768406b883567
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2020-12-18 23:37:51 +0100

    14

---
+++ 14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int a = arr[0];
+            int b = arr[1];
+            for (int i = a; i <= b - 2; i++)
+                for (int j = i + 1; j <= b - 1; j++)
+                    for (int k = j + 1; k<= b; k++)
+                        if (i*i + j * j == k * k)
+                            Console.WriteLine("{0} {1} {2}", i, j, k);
+        }
+    }
+}
