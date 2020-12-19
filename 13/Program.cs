3bd7c8fc7081802e6215d3eabb2ca735e37553ef
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2020-12-19 13:11:43 +0100

    13

---
+++ 13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+        int n = Convert.ToInt32(Console.ReadLine());
+            for (int j = 0; j<n - 1; j++) {
+                for (int k = 1; k<n; k++) {
+                    if (n - k == j) {
+                        Console.Write("X");
+                    }
+                    else{
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("X");
+                }
+
+            for (int l = 0; l<n; l++) {
+                Console.Write("X");
+            }
+        }
+    }
+ 
+}
